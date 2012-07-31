ff2b8ec87818e1644f8a4bce0aa0311d691e08de
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-07-31 14:15:23 +0100

    Add logging to leader-recorder
    Add info logging when connecting to the topic and when receiving a
    message.

--- src/recorders/leader.rb
+++ src/recorders/leader.rb
@@ -6,16 +6,19 @@ require 'bunny'
 module Recorders
   class LeaderRecorder
 
-    def initialize
+    def initialize(logger)
+      @logger = logger
       client = Bunny.new ENV['AMQP']
       client.start
       @queue = client.queue(ENV['QUEUE'] || 'leader') 
       exchange = client.exchange('datainsight', :type => :topic)
       @queue.bind(exchange, :key => '*.leader')
+      @logger.info("Bound to *.leader, listening for events")
     end
 
     def run
       @queue.subscribe do |msg|
+        @logger.info("Received message: #{msg[:payload]}")
         File.open('leader.json', 'w') do |handle|
           handle.puts msg[:payload]
         end
