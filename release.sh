a86edac229f1ea0c28786b0d6686bbae37d8bd99
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-08-03 15:39:23 +0100

    Add progress comments to release script
    @author Seema
    @author Rob

--- release.sh
+++ release.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+ANSI_YELLOW="\033[33m"
+ANSI_RED="\033[31m"
+ANSI_RESET="\033[0m"
+
 export VERSION="$1"
 if [ -z "$VERSION" ]; then
   echo "USAGE: release.sh <version-hash>"
@@ -13,8 +17,11 @@ HOST="deploy@datainsight.alphagov.co.uk"
 
 scp datainsight-recorder-narrative-$VERSION.zip $HOST:/srv/datainsight-recorder-narrative/packages
 # deploy
+echo -e "${ANSI_YELLOW}Deploying package${ANSI_RESET}"
 ssh $HOST "mkdir /srv/datainsight-recorder-narrative/release/$VERSION; unzip -o /srv/datainsight-recorder-narrative/packages/datainsight-recorder-narrative-$VERSION.zip -d /srv/datainsight-recorder-narrative/release/$VERSION;"
 # link
+echo -e "${ANSI_YELLOW}Linking package${ANSI_RESET}"
 ssh $HOST "rm /srv/datainsight-recorder-narrative/current; ln -s /srv/datainsight-recorder-narrative/release/$VERSION/ /srv/datainsight-recorder-narrative/current;"
 # restart
+echo -e "${ANSI_YELLOW}Restarting service${ANSI_RESET}"
 ssh $HOST "sudo service datainsight-recorder-narrative restart"
