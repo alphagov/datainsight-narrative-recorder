31ff417b70072246aa6ad59ae45503599e0ed7a8
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-09-24 15:18:54 +0100

    Remove old package, release and start scripts.
    author: @pbadenski
    author: @jthoenes

--- start-listener.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo -e "Starting listener"
-bundle exec bin/recorder run
--- start-web.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo -e "Starting application server"
-bundle exec unicorn --port $PORT
